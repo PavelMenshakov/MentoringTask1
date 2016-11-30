8b7b333e51ee79b66f9e9c847512e80e9a455154
PMenshakov <pmenshakov@gmail.com>
2016-11-30 13:18:40 +0300

    add styles

--- webpack-server.config.js
+++ webpack-server.config.js
@@ -1,9 +1,13 @@
 let path = require("path");
 let InlineEnviromentVariablesPlugin = require('inline-environment-variables-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	context: path.resolve("./src"),
-	entry: "./controllers/MainController",
+	entry: {
+		bundle: './controllers/MainController',
+		styles: './main.scss'
+	},
 	output: {
 		path: "./server",
 		filename: "bundle.js"  
@@ -11,10 +15,23 @@ module.exports = {
 	devServer: {
 		contentBase: './server'
 	},
-	plugins: [new InlineEnviromentVariablesPlugin({ NODE_ENV: "WEBPACK-SERVER_ENV"})],
+	plugins: [new InlineEnviromentVariablesPlugin({ NODE_ENV: "WEBPACK-SERVER_ENV"}),	
+			new ExtractTextPlugin('styles.css', {
+				allChunks: true
+			})
+	],
 	
 	module: {
-         loaders: [{
+         loaders: [ 
+			{
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+			{
              test: /\.js$/,
              exclude: /node_modules/,
 			 include: /src/,
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,34 @@
 let path = require("path");
 let InlineEnviromentVariablesPlugin = require('inline-environment-variables-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	context: path.resolve("./src"),
-	entry: "./controllers/MainController",
+	entry: {
+		bundle: './controllers/MainController',
+		styles: './main.scss'
+	},
 	output: {
 		path: "./dist",
 		filename: "bundle.js"  
 	},
-	plugins: [new InlineEnviromentVariablesPlugin({ NODE_ENV: "GIT-PAGE"})],
+	plugins: [new InlineEnviromentVariablesPlugin({ NODE_ENV: "GIT-PAGE"}),
+				new ExtractTextPlugin('styles.css', {
+				allChunks: true
+			})
+	],
 	
 	module: {
-         loaders: [{
+         loaders: [ 
+			{
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+			{
              test: /\.js$/,
              exclude: /node_modules/,
 			 include: /src/,
